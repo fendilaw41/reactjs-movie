06ac257764431dcd6de4606b39a16578489e0f22
fendilaw41 <fendilaw41@gmail.com>
2021-09-29 17:57:16 +0700

    fendi

--- src/components/ClassComponent.js
+++ src/components/ClassComponent.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
 const ClassComponent = (props) => {    // props ini adalah kiriman dari App.js
-    const movies = props.movies.map((movie, index) => {      // ini adalah looping di gabung dengan props dari App.js
+
+    const setFilter = props.movies.filter(i => i.Year <= 2015);
+    const newMovies = setFilter.map((movie, index) => {      // ini adalah looping di gabung dengan props dari App.js
         
       return <li key={index}>{movie.Title} - {movie.Year}</li>
     //   return console.log(movie.Title); // ini adalah array loopingan namun bukan sampai disini menampilkan data
     });
 
-    return <div>{movies}</div>; // kita butuh ini untuk menampilkan looping
+    return <div>{newMovies}</div>; // kita butuh ini untuk menampilkan looping
 }
 
 
